b2b27caa3034e2da1dded1ba40a0e642e218105b
sasezaki <sasezaki@gmail.com>
2020-12-12 19:35:35 +0900

    invalid directory_list

--- src/.phan/config.php
+++ src/.phan/config.php
@@ -347,7 +347,7 @@ return [
     // Thus, both first-party and third-party code being used by
     // your application should be included in this list.
     'directory_list' => [
-        'packages',
+        'src/packages',
         'vendor/phan/phan/src/Phan',
     ],
 
